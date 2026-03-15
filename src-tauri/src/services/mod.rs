f8e094acb8dc8496e31e2b9533aee95a3a1cb628
Evintkoo <evint.koo@gmail.com>
2026-03-16 00:27:38 +0700

    feat: remove auth_service module from Rust backend

--- src-tauri/src/services/mod.rs
+++ src-tauri/src/services/mod.rs
@@ -6,10 +6,7 @@ pub mod enhanced_market_service;
 pub mod openrouter_service;
 pub mod alpaca_service;
 pub mod fundamental_service;
-pub mod auth_service;
-
 pub use enhanced_market_service::EnhancedMarketDataService;
 pub use openrouter_service::OpenRouterService;
 pub use alpaca_service::AlpacaService;
 pub use fundamental_service::{FundamentalDataService, FundamentalMetrics};
-pub use auth_service::AuthService;
